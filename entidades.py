eb239336f0a78f214d3015d860814a69fd19f438
Alfredo Sánchez Sánchez <alfsan04@gmail.com>
2022-09-06 12:31:28 +0200

    Actualización con dos raquetas moviéndose

--- entidades.py
+++ entidades.py
@@ -27,4 +27,14 @@ class Raqueta:
     def dibujar(self, pantalla):
         pg.draw.rect(pantalla, self.color, (self.center_x - self.w // 2, self.center_y - self.h // 2, self.w, self.h))
 
+    def mover(self, tecla_arriba, tecla_abajo, y_max = 600):
+        estado_teclas = pg.key.get_pressed() #Devuelve una lista de todas las teclas con True si esta pulsada y False si no
+        if estado_teclas[tecla_arriba]:
+            self.center_y -= self.vy
+        if self.center_y < self.h // 2:
+            self.center_y = self.h // 2
+        if estado_teclas[tecla_abajo]:
+            self.center_y += self.vy
+        if self.center_y > y_max - self.h // 2:
+            self.center_y = y_max - self.h // 2
 
--- main.py
+++ main.py
@@ -5,16 +5,25 @@ pg.init()
 
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption("Pong")
+'creamos un cronometro obteniendo el objeto time.clock propio de pygame'
+cronometro = pg.time.Clock()
 
 game_over = False
 bola = Bola(400, 300, color = (255, 255, 255))
 raqueta1 = Raqueta(20, 300, w=20, h=120, color = (255, 255, 255))
 raqueta2 = Raqueta(780, 300, w=20, h=120, color = (255, 255, 255))
+raqueta1.vy = 5
+raqueta2.vy = 5
 
 while not game_over:
+    'Controlamos la tasa de refresco, hace 60 bucles por segundo, uno cada aprox 16 milisegundos'
+    dt = cronometro.tick(60)
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
+
+    raqueta1.mover(pg.K_w, pg.K_s)
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
 
     pantalla_principal.fill((0, 0, 0))
 
